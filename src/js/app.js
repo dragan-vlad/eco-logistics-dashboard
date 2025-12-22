3581650cd8e4bcc3262e442d92a6469802388811
Vlad Dragan <vladgdragan@gmail.com>
2025-12-22 05:44:54 +0200

    Changed the desing, the previous one was ugly to say the least and I've came up with another idea, which is more ambitious but more beautiful (when finished hopefully)

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,26 @@
 // Global
 
-const overlay = document.querySelector('.layer-overlay');
+$(document).ready(function() {
+    // 1. Inițializăm efectul
+    $('#ripple-container').ripples({
+        resolution: 512,
+        dropRadius: 10,
+        perturbance: 0.01,
+    });
 
-document.addEventListener('mousemove', (pos) => {
-    clientX = pos.clientX;
-    clientY = pos.clientY;
-    overlay.style.setProperty('--x', pos.clientX + 'px');
-    overlay.style.setProperty('--y', pos.clientY + 'px');
+    // 2. Automatism: "Pulsul Apei"
+    // Adăugăm aleatoriu mici onduleuri pentru a face apa să pară vie
+    setInterval(function() {
+        var $el = $('#ripple-container');
+        
+        // Alegem o poziție random pe X, dar pe Y o punem mai jos (aproape de noi)
+        // Ca să pară că vin dinspre privitor spre munte
+        var x = Math.random() * $el.outerWidth();
+        var y = $el.outerHeight() - (Math.random() * ($el.outerHeight() / 2));
+        
+        var dropRadius = 20;
+        var strength = 0.02 + Math.random() * 0.02;
+
+        $el.ripples('drop', x, y, dropRadius, strength);
+    }, 2000); // Se întâmplă o dată la 2 secunde
 });
